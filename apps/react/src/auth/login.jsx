241d03eae5de290a65503b72b06e5504b28968eb
Kushal <kushalkar007@gmail.com>
2026-01-11 17:01:42 +0530

    login page UX

--- apps/react/src/auth/login.jsx
+++ apps/react/src/auth/login.jsx
@@ -11,7 +11,63 @@ const Login = () => {
           <img src="login2.png" />
         </div>
       </div>
-      <div className="w-1/2 h-full bg-purple-50"></div>
+      <div className="w-1/2 h-full bg-purple-100 flex items-center justify-center">
+        <div className="bg-white p-10 rounded-xl shadow-2xl w-88 transform transition duration-500 hover:scale-105">
+          <h2 className="text-3xl font-bold text-center text-purple-700 mb-6">
+            Welcome Back 👋
+          </h2>
+          <form>
+            <div className="mb-4">
+              <input
+                type="text"
+                id="username"
+                placeholder="Enter username"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-purple-700"
+              />
+            </div>
+            <div className="mb-6">
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter password"
+                className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-purple-700"
+              />
+            </div>
+            <div className="flex items-center justify-between mb-6">
+              <label className="flex items-center text-gray-600">
+                <input
+                  type="checkbox"
+                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                />
+                <span className="ml-2 text-sm">Remember Me</span>
+              </label>
+              <a
+                href="#"
+                className="text-sm text-indigo-600 hover:underline font-medium"
+              >
+                Forgot Password?
+              </a>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-linear-to-r from-pink-600 via-purple-500 to-blue-600  text-white py-2 rounded-md font-bold shadow-lg hover:shadow-pink-500/50 hover:from-pink-600 hover:to-indigo-600 transition duration-300"
+            >
+              Login
+            </button>
+          </form>
+          <p className="text-sm text-gray-500 text-center mt-6">
+            Don't have an account?
+            <a
+              href="#"
+              className="text-indigo-600 font-semibold hover:underline"
+            >
+              Sign up
+            </a>
+          </p>
+        </div>
+        {/* </div> */}
+      </div>
     </div>
   );
 };
